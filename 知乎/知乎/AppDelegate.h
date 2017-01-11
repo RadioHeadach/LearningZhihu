3f6f6b270c412daa1d38b1325c4bb4a78ced13ba
linlifang <linlifang@roosters-mac.local>
2017-01-11 21:12:48 +0800

    Created a new project, deleted storyboard.

---
+++ 知乎/知乎/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  知乎
+//
+//  Created by ROOSTERLIN on 2017/1/11.
+//  Copyright © 2017年 ROOSTERLIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
